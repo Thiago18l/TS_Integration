5bcbd34678e562d84b8f1d77bd15dcd789ba5f17
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-15 02:30:59 -0300

    feat: app finish

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,22 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { ITodo, fetchTodos } from '../api/index';
+import { ITodo, fetchTodos, deleteTodo } from '../api/';
 import { IStore } from '../reducers/index';
 
 interface IAppProps {
     todos: ITodo[];
-    fetchTodos(): any;
+    fetchTodos: Function;
+    deleteTodo: Function;
 }
 
-interface IAccept {
-    mapState(): { todos: ITodo[] };
-    fetchTdos(): any;
+interface IAppState {
+    fetching: boolean;
 }
 
-class _App extends React.Component<IAppProps>{
+class _App extends React.Component<IAppProps, IAppState>{
+    constructor(props: IAppProps) {
+        super(props);
+
+        this.state = { fetching: false }; 
+    }
+
+    onTodoClick = (id: number): void => {
+        this.props.deleteTodo(id);
+    };
+
+    componentDidUpdate(prevProps: IAppProps): void {
+        if (!prevProps.todos.length && this.props.todos.length) {
+            this.setState({ fetching: false })
+        }
+    }
+
+    onButtonClick = (): void => {
+        this.props.fetchTodos();
+        this.setState({ fetching: true });
+    }
+    renderList = (): JSX.Element[] => {
+        return  this.props.todos.map((todo: ITodo) => {
+            return (
+                <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>{todo.title}</div>
+            );
+
+        })
+    }
     render() {
+        
         return (
-            <div>Hi</div>
+            <div>
+                <button onClick={this.onButtonClick}>FETCH</button>
+                { this.state.fetching ? "LOADING" : null }
+                {this.renderList()}
+            </div>
         );
     };
 };
@@ -27,5 +60,5 @@ const mapState = ( { todos }: IStore): { todos: ITodo[] } => {
 
 export const App = connect(
     mapState, 
-    { fetchTodos }
+    { fetchTodos, deleteTodo }
 )(_App);
