dd49937c8edcc5b4294c789a6807ada861f9bfb1
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-15 01:27:49 -0300

    feat: add redux to the component app

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { ITodo, fetchTodos } from '../api/index';
+import { IStore } from '../reducers/index';
 
-export class App extends React.Component {
+interface IAppProps {
+    todos: ITodo[];
+    fetchTodos(): any;
+}
+
+interface IAccept {
+    mapState(): { todos: ITodo[] };
+    fetchTdos(): any;
+}
+
+class _App extends React.Component<IAppProps>{
     render() {
         return (
-            <div>Hi There!</div>
+            <div>Hi</div>
         );
-    }
+    };
+};
+
+const mapState = ( { todos }: IStore): { todos: ITodo[] } => {
+    return { todos }
 }
+
+export const App = connect(
+    mapState, 
+    { fetchTodos }
+)(_App);
